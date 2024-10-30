9bb637bdd0e2aac53f466ea53f183e2df7e8d55c
T~kay <102425578+Tkay6677@users.noreply.github.com>
2024-10-30 19:58:49 +0000

    vite config

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,16 +1,22 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
-// https://vitejs.dev/config/
-export default defineConfig({
-  server: {
-    proxy: {
-      '/api': {
-        target: 'http://localhost:3000',
-        secure: false,
-      },
-    },
-  },
-
+export default defineConfig(({ mode }) => ({
   plugins: [react()],
-});
+  server: {
+    proxy:
+      mode === "development"
+        ? {
+            "/api": {
+              target: "http://localhost:3000",
+              secure: false,
+              changeOrigin: true,
+            },
+          }
+        : undefined,
+  },
+  preview: {
+    port: parseInt(process.env.PORT) || 3000,
+    host: "0.0.0.0",
+  },
+}));
